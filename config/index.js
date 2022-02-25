10ed5f50e703d3ce3308ed571b87db18b2d78a29
Nunofgg <goncalvesnuno00@gmail.com>
2022-02-25 15:39:44 +0000

    changes on the session

--- config/index.js
+++ config/index.js
@@ -57,9 +57,9 @@ module.exports = (app) => {
       }),
       cookie: {
         maxAge: 1000 * 60 * 60 * 24 * 365,
-        // sameSite: "none",
+        sameSite: "none",
         secure: false,
-        httpOnly: true,
+        httpOnly: false,
       },
     })
   );
