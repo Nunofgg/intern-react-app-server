d4af72f080a7f9f83ba08e3fad8df00ffe2c833c
Nunofgg <goncalvesnuno00@gmail.com>
2022-02-28 15:27:50 +0000

    changes on the session

--- config/index.js
+++ config/index.js
@@ -32,7 +32,11 @@ module.exports = (app) => {
 
   // controls a very specific header to pass headers from the frontend
   // ! please configure the cors `origin` key so that you can accept the requests wherever they might be coming from
-  app.use();
+  app.use(
+    cors({
+      credentials: 'include',
+    })
+  );
 
   // In development environment the app logs
   app.use(logger("dev"));
